3af9381ff5d57a1178c464898a57c544e423566a
Ivana Leban <ivanal123@gmail.com>
2018-04-19 06:02:09 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/schoolquiz/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.android.schoolquiz;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Context context = getApplicationContext();
+    CharSequence text = "Hello toast!";
+    int duration = Toast.LENGTH_SHORT;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+
+        });
+    }
+}
